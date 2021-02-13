6b7f9285d922a57562ec9f67398eac19fcfc87b5
Fredowisk <paulorobertoparreira@yahoo.com.br>
2021-02-12 21:55:43 -0300

    Uploading the app

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,13 @@
+const dark = {
+  title: 'dark',
+
+  colors: {
+    primary: '#513B3C',
+    secondary: '#C1EEFF',
+
+    background: '#222',
+    text: '#fff'
+  },
+}
+
+export default dark;
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,13 @@
+const light = {
+  title: 'light',
+
+  colors: {
+    primary: '#C1EEFF',
+    secondary: '#513B3C',
+
+    background: '#f5f5f5',
+    text: '#222'
+  },
+}
+
+export default light;
